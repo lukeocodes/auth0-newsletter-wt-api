f1b00a11bb8721d11de7d87417d390e288d6b247
Luke Oliff <luke@lukeoliff.com>
2018-02-22 14:09:59 +0000

    Create auth-0-newsletter-wt-api.js

---
+++ auth-0-newsletter-wt-api.js
@@ -0,0 +1,88 @@
+'use latest';
+import bodyParser from 'body-parser';
+import express from 'express';
+import Webtask from 'webtask-tools';
+import _ from 'lodash';
+
+const app = new express();
+app.use(bodyParser.json());
+
+const RESPONSE = {
+  OK : {
+    statusCode : 200,
+    message: "You have successfully subscribed to the newsletter!",
+  },
+  DUPLICATE : {
+    status : 400,
+    message : "You are already subscribed."
+  },
+  ERROR : {
+    statusCode : 400,
+    message: "Something went wrong. Please try again."
+  },
+  UNAUTHORIZED : {
+    statusCode : 401,
+    message : "You must be logged in to access this resource."
+  }
+};
+
+app.post('/subscribe', function(req, res){
+  var email = req.body.email;
+  if(email){
+    req.webtaskContext.storage.get(function(err, data){
+      if(err){
+        res.writeHead(400, { 'Content-Type': 'application/json'});
+        res.end(JSON.stringify(RESPONSE.ERROR));
+      }
+
+      data = data || [];
+
+      if(_.indexOf(data, email) == -1){
+        data.push(email);
+        req.webtaskContext.storage.set(data, function(err){
+          if(err){
+            res.writeHead(400, { 'Content-Type': 'application/json'});
+            res.end(JSON.stringify(RESPONSE.ERROR));
+          } else {
+            res.writeHead(200, { 'Content-Type': 'application/json'});
+            res.end(JSON.stringify(RESPONSE.OK));
+          }
+        })
+      } else {
+        res.writeHead(400, { 'Content-Type': 'application/json'});
+        res.end(JSON.stringify(RESPONSE.DUPLICATE));
+      }
+    })
+  } else {
+    res.writeHead(200, { 'Content-Type': 'application/json'});
+    res.end(JSON.stringify(RESPONSE.ERROR));
+  }
+})
+
+app.get('/subscribed/:email', function(req, res){
+  const email = req.params.email;
+  if(email){
+    req.webtaskContext.storage.get(function(err, data){
+      if(err){
+        res.writeHead(400, { 'Content-Type': 'application/json'});
+        res.end(JSON.stringify(RESPONSE.ERROR));
+      }
+
+      data = data || [];
+
+      if(_.indexOf(data, email) == -1){
+        res.writeHead(200, { 'Content-Type': 'application/json'});
+        res.end(JSON.stringify({subscribed: false}));
+      } else {
+        res.writeHead(200, { 'Content-Type': 'application/json'});
+        res.end(JSON.stringify({subscribed: true}));
+      }
+    })
+  } else {
+    res.writeHead(200, { 'Content-Type': 'application/json'});
+    res.end(JSON.stringify(RESPONSE.ERROR));
+  }
+})
+
+// Here we are exporting our express app using the wt helper library
+module.exports = Webtask.fromExpress(app);
