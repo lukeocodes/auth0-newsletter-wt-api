3a77810a482977e7fdf7527ca644f5f9233087d6
Luke Oliff <luke@lukeoliff.com>
2018-02-22 17:14:27 +0000

    Updating auth-0-newsletter-wt-api.js

--- auth-0-newsletter-wt-api.js
+++ auth-0-newsletter-wt-api.js
@@ -25,7 +25,7 @@ const RESPONSE = {
     message : "You must be logged in to access this resource."
   }
 };
- 
+
 app.post('/subscribe', function(req, res){
   var email = req.body.email;
   if(email){
